520bc3c2624c14e040add030b98e4985726eb9a4
yormng <1923001710@qq.com>
2021-08-18 19:01:13 +0800

    Initial commit

---
+++ conf/const.go
@@ -0,0 +1,9 @@
+package conf
+
+const (
+	MySQLHost     = "127.0.0.1"
+	MySQLPort     = "3306"
+	MySQLUser     = "root"
+	MySQLPassword = "000000"
+	MySQLDatabase = "sky"
+)
---
+++ database/mysql/connect.go
@@ -0,0 +1,69 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/dipper-mizar/sky/conf"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type InitParams struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+type ParamSetter func(*InitParams)
+
+func (params *InitParams) SetHost(host string) ParamSetter {
+	return func(params *InitParams) {
+		params.Host = host
+	}
+}
+
+func (params *InitParams) SetPort(port string) ParamSetter {
+	return func(params *InitParams) {
+		params.Port = port
+	}
+}
+
+func (params *InitParams) SetUser(user string) ParamSetter {
+	return func(params *InitParams) {
+		params.User = user
+	}
+}
+
+func (params *InitParams) SetPassword(password string) ParamSetter {
+	return func(params *InitParams) {
+		params.Password = password
+	}
+}
+
+func (params *InitParams) SetDatabase(database string) ParamSetter {
+	return func(params *InitParams) {
+		params.Database = database
+	}
+}
+
+func Init(functions ...ParamSetter) (*gorm.DB, error) {
+	params := &InitParams{
+		Host:     conf.MySQLHost,
+		Port:     conf.MySQLPort,
+		User:     conf.MySQLUser,
+		Password: conf.MySQLPassword,
+		Database: conf.MySQLDatabase,
+	}
+
+	dataSourceName := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", params.User,
+		params.Password, params.Host, params.Port, params.Database)
+	database, err := gorm.Open("mysql", dataSourceName)
+	if err != nil {
+		// TODO: Write error into logger.
+	}
+	for _, function := range functions {
+		function(params)
+	}
+	return database, nil
+}
